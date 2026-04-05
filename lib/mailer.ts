e49f85429b52c206f99781bba7d1fc1b6f258c33
Tech Guy <femithetechguy@gmail.com>
2026-04-04 20:09:27 -0400

    fix: resolve template conditionals in mailer, improve email color contrast

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -15,6 +15,14 @@ const transporter = nodemailer.createTransport({
 function loadTemplate(filename: string, vars: Record<string, string>): string {
   const filePath = path.join(process.cwd(), 'emails', filename)
   let html = fs.readFileSync(filePath, 'utf-8')
+
+  // Process {{#if var}}...{{else}}...{{/if}} and {{#if var}}...{{/if}} conditionals
+  html = html.replace(
+    /\{\{#if (\w+)\}\}([\s\S]*?)(?:\{\{else\}\}([\s\S]*?))?\{\{\/if\}\}/g,
+    (_, key, truthy, falsy = '') => (vars[key] ? truthy : falsy)
+  )
+
+  // Simple {{var}} replacement
   for (const [key, value] of Object.entries(vars)) {
     html = html.replaceAll(`{{${key}}}`, value ?? '')
   }
